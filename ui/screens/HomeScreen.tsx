5e024010400e3744ea031d607c42a828becdfd6d
Maria Lundhammar <marialundhammar@Marias-MBP.localdomain>
2023-01-30 13:10:00 +0100

    added some styling for button

--- ui/screens/HomeScreen.tsx
+++ ui/screens/HomeScreen.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { View, Pressable, Text } from 'react-native';
+import styleButtons from '../styles/styleButtons';
 
-const App = ({ navigation }) => {
-  const onPress = () => {
-    navigation.navigate('Map');
-    console.log('helloooo');
-  };
+const HomeScreen = ({ navigation }) => {
   return (
     <View
       style={{
         width: '100%',
         height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Pressable onPress={() => navigation.navigate('Map')}>
-        <Text> Check this map out </Text>
+      <Pressable
+        style={styleButtons.buttonDefault}
+        onPress={() => navigation.navigate('Map')}
+      >
+        <Text style={styleButtons.buttonDefaultText}> Check this map out </Text>
       </Pressable>
     </View>
   );
 };
 
-export default App;
+export default HomeScreen;
--- ui/styles/styleButtons.tsx
+++ ui/styles/styleButtons.tsx
@@ -1,11 +1,21 @@
 import { StyleSheet } from 'react-native-web';
 
 const styleButtons = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'lightgrey',
+  //Buttons
+  buttonDefault: {
+    width: 160,
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: '#68a0cf',
+    borderColor: 'black',
+    borderWidth: 2,
+    padding: 8,
+  },
+
+  //ButtonText
+  buttonDefaultText: {
+    fontWeight: 'bold',
   },
 });
 
